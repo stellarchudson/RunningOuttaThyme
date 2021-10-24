66058d46635a929cc0d84b74dd765587ac83a0fc
Caileigh <caileigh.hudson@colorado.edu>
2021-10-24 12:13:21 -0600

    added energy bar

---
+++ Thyme/Assets/findPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class findPlayer : MonoBehaviour
+{
+    
+        [SerializeField] public Transform player;
+
+        [SerializeField] public Animator animator;
+        void Update()
+        {
+            if (Vector2.Distance(transform.position, player.position) < 3)
+            {
+                animator.SetBool("isAttack", true);
+            
+            }
+
+            else
+            {
+                animator.SetBool("isAttack", false);
+            }
+        }
+    }
+
---
+++ Thyme/Assets/healthBar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour
+{
+
+    public Slider energy;
+    public Gradient gradient;
+    public Image fill;
+
+    public void MaxHealth(int coffee)
+    {
+        energy.maxValue = coffee;
+        energy.value = coffee;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetEnergy(int coffee)
+    {
+        energy.value = coffee;
+        fill.color = gradient.Evaluate(energy.normalizedValue);
+    }
+
+}
